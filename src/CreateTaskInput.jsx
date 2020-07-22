0e51925214c856e269d2a1f8a46e1a9b2ae06521
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-07-22 22:36:20 +0300

    todo

---
+++ src/CreateTaskInput.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+class CreateTask extends React.Component {
+  state = {
+    value: ''
+  }
+
+   handleChange = (e) => {
+    this.setState({
+      value: e.target.value
+    });
+  }
+
+  onCreateTask = () => {
+    this.props.createTask(this.state.value);
+    this.setState({
+      value: ''
+    })
+  }
+
+  render() {
+    return (
+        <div className="create-task">
+          <input className="create-task__input"
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.value}
+          />
+          <button className="btn create-task__btn"
+          onClick={this.onCreateTask}
+          >Create</button>
+        </div>
+    );
+  } 
+};
+
+CreateTask.propTypes = {
+  createTask: PropTypes.func.isRequired
+};
+
+export default CreateTask;
---
+++ src/Task.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+const Task = ({id, done, text, onToggleTask, onDelete}) => {
+  return (
+    <li className={classNames('list-item', {'list-item_done' : done})}>
+      <input
+        onClick={() => onToggleTask(id)}
+        defaultChecked={done}
+        type="checkbox"
+        className="list-item__checkbox" />
+      <span className="list-item__text">{text}</span>
+      <button
+        onClick={() => onDelete(id)}
+        className="list-item__delete-btn" />
+    </li>
+  );
+};
+
+Task.propTypes = {
+  id: PropTypes.string.isRequired,
+  done: PropTypes.bool,
+  text: PropTypes.string,
+  onToggleTask: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired
+};
+
+Task.defaultProps = {
+  done: false,
+  text: ''
+}
+
+export default Task;
